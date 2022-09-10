ced3b7a25bab8700bfdb663033897905b6d6acb5
GeraldineObetta <geraldine.obetta.pg78810@unn.edu.ng>
2022-09-10 00:24:25 -0700

    8 Print base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,52 +1,23 @@
 #include <stdio.h>
-
-
-
 /**
  *
- *   * main - Prints a hexadecimal string
- *
- *     *
- *
- *       * Return: Always (Success)
- *
- *         */
-
+ * main - Prints a hexadecimal string
+ * 
+ * Return: Always zero
+ */
 int main(void)
-
 {
+	char c;
 
-		char c;
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
 
-
-
-			for (c = '0'; c <= '9'; c++)
-
-					{
-
-								putchar(c);
-
-									}
-
-
-
-				for (c = 'a'; c <= 'f'; c++)
-
-						{
-
-									putchar(c);
-
-										}
-
-
-
-					putchar('\n');
-
-
-
-						return (0);
-
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
 }
-
-
-
