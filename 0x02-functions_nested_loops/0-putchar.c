77b8bef51537ecf31aae1695b442ec02aeb492e7
GeraldineObetta <geraldine.obetta.pg78810@unn.edu.ng>
2022-09-13 13:51:35 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,14 +2,13 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - print putchar                                                                                                                 
- *
- * Return: Always returns 0                                                                                                             
- */                                                                                                                                     
-int main(void)                                                                                                                          
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
 {
 
-printf("_putchar\n");                                                                                                                 
+printf("_putchar\n");
 
-return (0);                                                                                                                     
+return (0);
 }
