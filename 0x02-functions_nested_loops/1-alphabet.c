270021b8e4bfe3dcc04697e84b7464d261a3cb8f
GeraldineObetta <geraldine.obetta.pg78810@unn.edu.ng>
2022-09-13 14:12:21 -0700

    1 alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= '2'; c++)
+	{
+		_putchar(c);
+	}
+_putchar('\n');
+}
