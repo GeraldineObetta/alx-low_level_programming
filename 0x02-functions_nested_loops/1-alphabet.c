a74c89f848f45dc1efae5d857a978773d5af0ac8
GeraldineObetta <geraldine.obetta.pg78810@unn.edu.ng>
2022-09-13 14:49:26 -0700

    1alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,12 @@
 #include "main.h"
-
 /**
- * main - Entry point
+ * print_alphabet - functo to print abc
  *
  * Return: Always 0
  */
 void print_alphabet(void)
 {
 	char c;
-
 	for (c = 'a'; c <= '2'; c++)
 	{
 		_putchar(c);
